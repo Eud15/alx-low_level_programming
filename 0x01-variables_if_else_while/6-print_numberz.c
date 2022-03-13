6db2bf94c6ba94dae7fc9d88f9ea2958afe49c0e
root <Eud15>
2022-03-13 11:15:54 -0700

    print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - start of the program *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		putchar (i + '0');
+	putchar('\n');
+	return (0);
+}
