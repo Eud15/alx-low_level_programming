ee4ac39a6da0fbd5595d3b91346d4a09bcd27bdf
root <Eud15>
2022-03-13 09:53:06 -0700

    last digital

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	int n;
 	int last_digit;
-	srand(time(0));
 
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	printf("Last digit of %i is %i", n, last_digit);
