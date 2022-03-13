ffec1255e6e605f3efc53525e8f838c6f7df6136
root <Eud15>
2022-03-13 12:41:05 -0700

    print base 16

---
+++ 8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - starts the program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	char c;
+	int i;
+	for (i = 0; i < 10; i++)
+		putchar (i + '0');
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+C programming at ALX school
