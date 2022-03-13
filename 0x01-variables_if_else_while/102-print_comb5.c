90855cdeba2afbea87f3ca21a47457496f2ad89e
root <Eud15>
2022-03-13 13:44:40 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,8 @@ int main(void)
 			putchar (i % 10 + '0');
 			putchar (' ');
 			putchar (j / 10 + '0');
-			putchar (j % 10 + '0'); if (i == 98 && j == 99)
+			putchar (j % 10 + '0');
+			if (i == 98 && j == 99)
 				break;
 			putchar (',');
 			putchar (' ');
