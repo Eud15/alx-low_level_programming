9766b731c9d7541e4c8a8bbe5c2698be3a0fe678
root <Eud15>
2022-03-13 13:42:53 -0700

    print comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - starts the program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 99; i++)
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar (i / 10 + '0');
+			putchar (i % 10 + '0');
+			putchar (' ');
+			putchar (j / 10 + '0');
+			putchar (j % 10 + '0'); if (i == 98 && j == 99)
+				break;
+			putchar (',');
+			putchar (' ');
+		}
+	putchar ('\n');
+	return (0);
+}
