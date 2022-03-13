ac891f88016c8be53387867303b9193ff5ddeca5
root <Eud15>
2022-03-13 09:57:23 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - start of program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+	return (0);
+}
