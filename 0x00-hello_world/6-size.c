8e701685b0cfd067621fa36c72bee2e746af8bdd
root <Eud15>
2022-03-13 14:07:30 -0700

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,5 +9,6 @@ int main(void)
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long));
-	printf("Size of a float: %i byte(s)\n", sizeof(float)); return (0);
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
