edfc21d8e4122b17fd3a2e6cc24c222ca796a2bc
root <Eud15>
2022-03-13 10:48:48 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ *
+ * Return: nothing if successful
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
