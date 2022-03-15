1a2a27641793dc93c07bed8b7e16b6d6a6e74936
root <Eud15>
2022-03-15 23:45:55 +0100

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,53 +1,55 @@
 #include "main.h"
 
 /**
- * print_times_table - print n times table starting at 0
- * @n: takes number between 0 and 15
- *
- * Return: nothing
+ * print_times_table - prints times table for numbers from 0-14
+ * @n: An input integer value
+ * Return: Nothing
  */
 void print_times_table(int n)
 {
-	int column;
-	int row;
-	int hundreds;
-	int tens;
-	int ones;
-	int mult;
+	int i, j;
 
-	if (n >= 0 && n <= 15)
+	if (n > 0 && n < 15)
 	{
-		for (row = 0; row <= n; row++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			for (column = 1; column <= n; column++)
-			{
-				mult = column * row;
-				hundreds = mult / 100;
-				tens = ((mult / 10) % 10);
-				ones = mult % 10;
-				_putchar (',');
-				_putchar (' ');
-				if (mult < 10)
-				{
-					_putchar (' ');
-					_putchar (' ');
-					_putchar (ones + '0');
-				}
-				else if (mult > 9 && mult < 100)
-				{
-					_putchar (' ');
-					_putchar (tens + '0');
-					_putchar (ones + '0');
-				}
-				else if (mult > 99)
-				{
-					_putchar (hundreds + '0');
-					_putchar (tens + '0');
-					_putchar (ones + '0');
-				}
-			}
-			_putchar ('\n');
+			for (j = 1; j <= n; j++)
+				putformat(i * j);
+			_putchar('\n');
 		}
+	}
+}
+
+/**
+ * putformat - formatted characters to output
+ * @n: number to format
+ * Return: nothing
+ */
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 }
