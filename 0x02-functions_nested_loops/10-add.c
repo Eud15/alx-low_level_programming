1225a935343a884804057177fabfd33e3e55cc02
root <Eud15>
2022-03-15 20:08:39 +0100

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds 2 numbers together
+ * @a: first number
+ * @b: second number
+ *
+ * Return: nothing
+ */
+int add(int a, int b)
+{
+	int sums;
+
+	sums = a + b;
+
+	return (sums);
+}
