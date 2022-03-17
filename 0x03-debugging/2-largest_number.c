2f1e10e333fd245674fe2a6ef5e512fbefaf0a72
root <Eud15>
2022-03-17 20:10:59 +0100

    2 largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,15 +9,15 @@
  */
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	 int largest;
 
 	if (a >= b && a >= c)
 	{
-		largest = a;
-	}
-	else if (b >= a && b >= c)
-	{
 		largest = b;
+	}
+	else if (c >= a && c >= b)
+	{
+		largest = a;
 	}
 	else if (c >= a && c >= b)
 	{
