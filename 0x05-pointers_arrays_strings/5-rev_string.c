a5517fb551f50846695cbb3afe9547cd6323a47e
root <Eud15>
2022-03-21 14:46:10 +0100

    5 string rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,31 +1,42 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * rev_string - reverses a string
- * @s: holi
+ * _strlen - returns the length of a given string
  *
+ *
+ * @s: string to check the length of
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+  int i;
+
+  i = 0;
+  while (s[i] != '\0')
+    i++;
+  return (i);
+}
+
+
+/**
+ * rev_string - reverses string in place, without printing it
+ *
+ * @s: string to reverse
+ * Return: void
  */
 void rev_string(char *s)
 {
-	int i;
-	int j;
-	int temp;
-	int length;
+  int i, len;
+  char a, z;
 
-	while (s[i] != 0)
-	{
-		i++;
-	}
-
-	length = i;
-	i = 0;
-	j = length - 1;
-
-	while (i < j)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-	}
+  len = _strlen(s) - 1;
+  i = 0;
+  while (i < len)
+    {
+      a = s[i];
+      z = s[len];
+      s[i++] = z;
+      s[len--] = a;
+    }
 }
